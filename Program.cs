674b2b4096aa07e9c84d05147551f863cef84ecb
Hemory Phifer <hphife@yahoo.com>
2020-09-25 17:01:29 -0400

    Create Program.cs
    to be paired with using a getter and setter method

---
+++ Program.cs
@@ -0,0 +1,11 @@
+           //todo to be paired with using a getter and setter method
+
+            Student s1 = new Student();
+            s1.SetName("Hem");
+            s1.SetAge(22);
+
+            Console.WriteLine(s1.GetName());
+            Console.WriteLine(s1.GetAge());
+          
+
+            Console.ReadLine();
